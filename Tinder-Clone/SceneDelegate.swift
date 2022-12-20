fe679bbf6fa4ae4fc474653711913dd7015e6c0d
nsemirdev <nsemirdev@gmail.com>
2022-12-20 19:15:30 +0300

    initialize xcode project

---
+++ Tinder-Clone/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Tinder-Clone
+//
+//  Created by Emir Alkal on 20.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController()
+    }
+}
+
